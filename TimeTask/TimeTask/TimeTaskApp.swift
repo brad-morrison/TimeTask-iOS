6d310df238927e24ca9cf12ecc26cf4e88d9f6b6
Bradley Morrison <45197952+brad-morrison@users.noreply.github.com>
2022-09-12 12:32:24 +0100

    Created Xcode project

---
+++ TimeTask/TimeTask/TimeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTaskApp.swift
+//  TimeTask
+//
+//  Created by Bradley Morrison on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
