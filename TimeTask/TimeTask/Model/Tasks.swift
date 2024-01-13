6ca8b21eb479fc08b3a9c3d3e8423662e43272b3
brad-morrison <bradley_morrison@outlook.com>
2024-01-13 02:18:28 +0000

    Updated tasks for demo

--- TimeTask/TimeTask/Model/Tasks.swift
+++ TimeTask/TimeTask/Model/Tasks.swift
@@ -18,11 +18,11 @@ struct Task: Identifiable {
 }
 
 var tasks = [
-    Task(title: "Tidying", icon: "🐦", colorA: Color(hex: "43CEA2"), colorB: Color(hex: "185A9D"), time: 30, on: true),
-    Task(title: "Eating", icon: "🙈", colorA: Color(hex: "757F9A"), colorB: Color(hex: "D7DDE8"), time: 60, on: false),
-    Task(title: "Sleeping", icon: "🦋", colorA: Color(hex: "5C258D"), colorB: Color(hex: "4389A2"), time: 90, on: false),
-    Task(title: "Web Design", icon: "🌸", colorA: Color(hex: "134E5E"), colorB: Color(hex: "71B280"), time: 120, on: false),
-    Task(title: "Social Media", icon: "🍀", colorA: Color(hex: "757F9A"), colorB: Color(hex: "D7DDE8"), time: 19800, on: false),
-    Task(title: "Social Media", icon: "🦋", colorA: Color(hex: "614385"), colorB: Color(hex: "516395"), time: 80420, on: false),
-    Task(title: "Social Media", icon: "🐦", colorA: Color(hex: "134E5E"), colorB: Color(hex: "71B280"), time: 0, on: false)
+    Task(title: "Programming", icon: "👩🏻‍💻", colorA: Color(hex: "43CEA2"), colorB: Color(hex: "185A9D"), time: 30, on: true),
+    Task(title: "Eating", icon: "🍔", colorA: Color(hex: "757F9A"), colorB: Color(hex: "D7DDE8"), time: 60, on: false),
+    Task(title: "Sleeping", icon: "😴", colorA: Color(hex: "5C258D"), colorB: Color(hex: "4389A2"), time: 90, on: false),
+    Task(title: "Gardening", icon: "🌸", colorA: Color(hex: "134E5E"), colorB: Color(hex: "71B280"), time: 120, on: false),
+    Task(title: "Web Design", icon: "🖌️", colorA: Color(hex: "757F9A"), colorB: Color(hex: "D7DDE8"), time: 19800, on: false),
+    Task(title: "Reading", icon: "📚", colorA: Color(hex: "614385"), colorB: Color(hex: "516395"), time: 80420, on: false),
+    Task(title: "Coffee", icon: "☕️", colorA: Color(hex: "134E5E"), colorB: Color(hex: "71B280"), time: 0, on: false)
 ]
